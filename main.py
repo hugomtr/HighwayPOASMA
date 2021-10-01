d2d28e139e9448580743bba139d7f76fd133840b
hugomtr <hugomaitre340@gmail.com>
2021-10-01 15:31:29 +0200

    seance1

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ def loadenvironment():
     créer la liste chainée rempli de 0 
     """
 
+def insertExitVehicle():
+    """
+    initialise à chaque epoch une insertion de plusieurs votures
+    """
 
 def nextEpoch():
     """
     load next state
     """
+
 
